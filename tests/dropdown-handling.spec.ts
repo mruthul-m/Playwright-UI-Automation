c70cccf6936317ab257eb075d4d5dc666ed73524
mruthul-m <memruthu@gmail.com>
2026-06-26 20:35:17 +0530

    minor fix

--- tests/dropdown-handling.spec.ts
+++ tests/dropdown-handling.spec.ts
@@ -4,5 +4,5 @@ test('Handling Drop Downs', async ({page}) => {
     await page.goto('https://the-internet.herokuapp.com/dropdown');
     const select = page.locator('css=select#dropdown')
     await select.selectOption('1');
-    await expect(select.locator('option:checked')).toHaveText('Option 2')
+    await expect(select.locator('option:checked')).toHaveText('Option 1')
 })
